42bef416e3e5e15fc5e1a628c7f368fedb83261f
Francja <adambielewicz92@gmail.com>
2024-10-04 22:53:11 +0200

    Added classes with a preliminary structure

--- car_rental_system.py
+++ car_rental_system.py
@@ -1,3 +1,41 @@
+import datetime
+
+
+# Car class with the car type and number of cars available:
+class Car:
+    def __init__(self, car_type, count):
+        self.car_type = car_type
+        self.count = count
+
+
+# Reservation class to store reservation details
+class Reservation:
+    _id_num = 1
+
+    def __init__(self, car_type, start_date, number_of_days):
+        self.car_type = car_type
+        self.start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d %H:%M")
+        self.number_of_days = number_of_days
+        self.reservation_id = Reservation._id_num
+        Reservation._id_num += 1
+
+
+# CarRentalSystem class to manage cars and reservations
+class CarRentalSystem:
+    def __init__(self):
+        self.cars = []
+        self.reservations = []
+
+    def reserve_car(self):
+        print("\nCar reserved")
+
+    def show_reservations(self):
+        print("\nShowing reservations")
+
+    def remove_reservation(self):
+        print("\nRemoved reservation")
+
+
 def menu():
     print("\nCar Rental System Menu:",
             "1. Reserve a car",
@@ -8,15 +46,18 @@ def menu():
 
 
 if __name__ == '__main__':
+
+    crs = CarRentalSystem()
+
     while True:
         menu()
-        choice = input("Please choose an option: ")
+        choice = input("\nPlease choose an option: ")
         if choice == '1':
-            print("\nCreated a reservation.")
+            crs.reserve_car()
         elif choice == '2':
-            print("\nShowing reservations:")
+            crs.show_reservations()
         elif choice == '3':
-            print("\nRemoved a reservation.")
+            crs.remove_reservation()
         elif choice == '4':
             print("\nClosing the application.")
             break
