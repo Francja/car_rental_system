8dc188ea85636bf74f85e9678dd921149fb7b8d5
Francja <adambielewicz92@gmail.com>
2024-10-05 00:31:58 +0200

    Checking for conflicting reservations using dates and available cars.

--- car_rental_system.py
+++ car_rental_system.py
@@ -17,8 +17,9 @@ class Reservation:
 
     def __init__(self, car_type, start_date, number_of_days):
         self.car_type = car_type
-        self.start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d %H:%M")
+        self.start_date = start_date
         self.number_of_days = number_of_days
+        self.end_date = self.start_date + datetime.timedelta(days=number_of_days)
         self.reservation_id = Reservation._id_num
         Reservation._id_num += 1
 
@@ -39,13 +40,34 @@ class CarRentalSystem:
             print(car)
 
     def reserve_car(self, car_type, start_date, number_of_days):
-        for car in self.cars:
-            if car.car_type == car_type and car.count > 0:
-                new_reservation = Reservation(car_type,start_date,number_of_days)
-                self.reservations.append(new_reservation)
-                car.count -= 1
-                return Reservation
+        start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d %H:%M")
+        number_of_days = int(number_of_days)
+        if not self.is_car_available(car_type, start_date,number_of_days):
+            print("No cars available for this dates")
+        else:
+            for car in self.cars:
+                if car.car_type == car_type and car.count > 0:
+                    new_reservation = Reservation(car_type,start_date,number_of_days)
+                    self.reservations.append(new_reservation)
+                    return Reservation
 
+    def is_car_available(self, car_type, start_date, number_of_days):
+
+        end_date = start_date + datetime.timedelta(days=number_of_days)
+
+        for car in self.cars:
+            if car.car_type == car_type:
+                total_cars_available = car.count
+
+        conflicting_reservations = 0
+        for reservation in self.reservations:
+            if reservation.car_type == car_type:
+                if reservation.start_date < end_date and reservation.end_date > start_date:
+                    conflicting_reservations += 1
+
+        available_cars = total_cars_available - conflicting_reservations
+
+        return available_cars > 0
 
     def show_reservations(self):
         for reservation in self.reservations:
@@ -71,6 +93,10 @@ if __name__ == '__main__':
     crs.add_cars("SUV", 1)
     crs.add_cars("Van", 3)
     crs.show_cars()
+    print("First reserv")
+    crs.reserve_car("SUV", "2023-03-01 10:00", 5)
+    print("Second reserv.")
+    crs.reserve_car("SUV", "2023-03-06 13:00", 5)
 
     while True:
         menu()
@@ -78,7 +104,7 @@ if __name__ == '__main__':
         if choice == '1':
             car_type = input("Enter car type: ")
             start_date = input("Enter start date (YYYY-MM-DD HH:MM): ")
-            number_of_days = int(input("Enter number of days: "))
+            number_of_days = input("Enter number of days: ")
             crs.reserve_car(car_type, start_date, number_of_days)
         elif choice == '2':
             crs.show_reservations()
