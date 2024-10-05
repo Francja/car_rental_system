3e89bdd8f2f2ad74fef8e1f51f5123a529a25ef2
Francja <adambielewicz92@gmail.com>
2024-10-05 12:05:15 +0200

    Added sweep line algorithm to better check collisions of car reservations

--- car_rental_system.py
+++ car_rental_system.py
@@ -45,60 +45,91 @@ class CarRentalSystem:
         start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d %H:%M")
         number_of_days = int(number_of_days)
         if not self.is_car_available(car_type, start_date, number_of_days):
-            print("\nNo cars available for this date")
+            print("\nNo cars available for this type and date")
         else:
-            for car in self.cars:
-                if car.car_type == car_type and car.count > 0:
-                    new_reservation = Reservation(car_type, start_date, number_of_days)
-                    self.reservations.append(new_reservation)
-                    print(f"\nNew reservation: {new_reservation}")
+            new_reservation = Reservation(car_type, start_date, number_of_days)
+            self.reservations.append(new_reservation)
+            print(f"\nNew reservation: {new_reservation}")
 
     def is_car_available(self, car_type, start_date, number_of_days):
 
         end_date = start_date + datetime.timedelta(days=number_of_days)
 
+        # Find total cars available for the type requested
+        total_cars_available = 0
         for car in self.cars:
             if car.car_type == car_type:
                 total_cars_available = car.count
 
-        conflicting_reservations = 0
+        # Create a list of events (start of reservation: +1, end of reservation: -1)
+        events = []
         for reservation in self.reservations:
             if reservation.car_type == car_type:
-                if reservation.start_date < end_date and reservation.end_date > start_date:
-                    conflicting_reservations += 1
+                # Start of reservation
+                events.append((reservation.start_date, 1))
+                # End of reservation
+                events.append((reservation.end_date, -1))
 
-        available_cars = total_cars_available - conflicting_reservations
+        # Add currently made reservation to the list of events:
+        events.append((start_date, 1))
+        events.append((end_date, -1))
 
-        return available_cars > 0
+        # Sort events by time, then by value if they collide
+        # so cars can end/begin reservation at the same time
+        events.sort(key=lambda element: (element[0], element[1]))
+
+        # Sweep through already existing and new reservation and check
+        #  if it exceeds the number of cars available at any moment
+        active_reservations = 0
+        for time, value in events:
+            active_reservations += value
+            if active_reservations > total_cars_available:
+                return False
+        return True
 
     def show_reservations(self):
         for reservation in self.reservations:
             print(reservation)
 
-    def remove_reservation(self):
-        print("\nRemoved reservation")
+    def remove_reservation(self, reservation_id):
+        reservation_id = int(reservation_id)
+        for reservation in self.reservations:
+            if reservation.reservation_id == reservation_id:
+                self.reservations.remove(reservation)
+        print(f"\nReservation {reservation_id} removed.")
 
 
 def menu():
     print("\nCar Rental System Menu:",
             "1. Reserve a car",
-            "2. Show all reservations",
-            "3. Remove a reservation",
-            "4. Exit",
+            "2. Show cars available",
+            "3. Show all reservations",
+            "4. Remove a reservation",
+            "5. Exit",
             sep="\n")
 
 
 if __name__ == '__main__':
 
+
     crs = CarRentalSystem()
     crs.add_cars("Sedan", 1)
     crs.add_cars("SUV", 2)
     crs.add_cars("Van", 3)
+
+    # basic unit testing for now
     crs.show_cars()
-    crs.reserve_car("SUV", "2023-03-01 10:00", 5)
+    crs.reserve_car("SUV", "2023-03-01 13:00", 5)
+    crs.reserve_car("Sedan", "2023-03-01 13:00", 5)
+    crs.reserve_car("Sedan", "2023-03-01 13:00", 5)
+    crs.reserve_car("Ford Focus", "2023-03-01 13:00", 5)
     crs.reserve_car("SUV", "2023-03-06 13:00", 5)
     crs.reserve_car("SUV", "2023-03-03 12:00", 5)
     crs.reserve_car("SUV", "2023-03-03 12:00", 5)
+    crs.show_reservations()
+    crs.remove_reservation(1)
+    crs.reserve_car("SUV", "2023-03-03 12:00", 1)
+    crs.show_reservations()
 
     while True:
         menu()
@@ -109,10 +140,13 @@ if __name__ == '__main__':
             number_of_days = input("Enter number of days: ")
             crs.reserve_car(car_type, start_date, number_of_days)
         elif choice == '2':
-            crs.show_reservations()
+            crs.show_cars()
         elif choice == '3':
-            crs.remove_reservation()
+            crs.show_reservations()
         elif choice == '4':
+            reservation_id = input("Enter reservation ID to be removed: ")
+            crs.remove_reservation(reservation_id)
+        elif choice == '5':
             print("\nClosing the application.")
             break
         else:
